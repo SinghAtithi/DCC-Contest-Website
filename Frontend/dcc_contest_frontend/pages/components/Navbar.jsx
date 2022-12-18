dccc02b9d8de8f1f796fcd9211643bce9f03ab0a
SinghAtithi <mails4rockstararyan@gmail.com>
2022-12-19 02:48:13 +0530

    L to l

--- Frontend/dcc_contest_frontend/pages/components/Navbar.jsx
+++ Frontend/dcc_contest_frontend/pages/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import Image from "next/image"; 
 
 function Navbar() {
--- Frontend/dcc_contest_frontend/pages/index.js
+++ Frontend/dcc_contest_frontend/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import Navbar from './components/Navbar'
 
 import HomePage from './HomePage/Home' 
