6e39c3e4dbb55c526772bd589e50e7e9a2fe5d24
SinghAtithi <mails4rockstararyan@gmail.com>
2022-12-19 12:38:39 +0530

    added basic boilerplate

---
+++ Frontend/dcc_contest_frontend/pages/Contests.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contests() {
+  return (
+    <div>Contests</div>
+  )
+}
+
+export default Contests
---
+++ Frontend/dcc_contest_frontend/pages/components/Question.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Question() {
+  return (
+    <div>Question</div>
+  )
+}
+
+export default Question
