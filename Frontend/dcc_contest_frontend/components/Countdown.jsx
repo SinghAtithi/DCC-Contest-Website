b630f06c3e0fed52f36ead4a42ec0bf66a79f853
SinghAtithi <mails4rockstararyan@gmail.com>
2023-04-17 23:38:13 +0530

    Contest Page changes

--- Frontend/dcc_contest_frontend/components/Countdown.jsx
+++ Frontend/dcc_contest_frontend/components/Countdown.jsx
@@ -1,68 +1,63 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import moment from 'moment/moment';
+import React from "react";
+import { useState, useEffect } from "react";
+import moment from "moment/moment";
 
 export default function Countdown(props) {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
-    const [days, setDays] = useState(0);
-    const [hours, setHours] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
+  const getTime = () => {
+    const deadline = moment(props.deadline, "DD/MM/YYYY h:mm:s");
+    var now = moment(Date.now()).format("DD/MM/YYYY h:mm:s");
+    now = moment(now, "DD/MM/YYYY h:mm:s");
 
+    const time = deadline.diff(now);
+    // console.log(time);
 
-    const getTime = () => {
+    setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+  };
 
-        const deadline = moment(props.deadline,'DD/MM/YYYY h:mm:s');
-        var now = moment(Date.now()).format('DD/MM/YYYY h:mm:s');
-        now = moment(now,'DD/MM/YYYY h:mm:s');
-        
+  useEffect(() => {
+    const interval = setInterval(() => getTime(), 1000);
 
-        const time = deadline.diff(now);
-        // console.log(time);
+    return () => clearInterval(interval);
+  }, []);
 
-        setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
-        setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
-        setMinutes(Math.floor((time / 1000 / 60) % 60));
-        setSeconds(Math.floor((time / 1000) % 60));
-    };
-
-    useEffect(() => {
-        const interval = setInterval(() => getTime(), 1000);
-
-        return () => clearInterval(interval);
-    }, []);
-
-
-    return (
-        <>
-            <div className="badge badge-info gap-2 rounded-3xl">
-                <div className="flex gap-5 align-middle justify-center">
-                    <div className=' text-zinc-900'>
-                        <span className="countdown font-mono text-lg">
-                            <span style={{ "--value": days }}></span>
-                        </span>
-                        days
-                    </div>
-                    <div className=' text-zinc-900'>
-                        <span className="countdown font-mono text-lg">
-                            <span style={{ "--value": hours }}></span>
-                        </span>
-                        hours
-                    </div>
-                    <div className=' text-zinc-900'>
-                        <span className="countdown font-mono text-lg">
-                            <span style={{ "--value": minutes }}></span>
-                        </span>
-                        min
-                    </div>
-                    <div className=' text-zinc-900'>
-                        <span className="countdown font-mono text-lg">
-                            <span style={{ "--value": seconds }}></span>
-                        </span>
-                        sec
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="border border-blue-400 min-h-8 h-8">
+        <div className="flex gap-5 align-middle justify-center">
+          <div className=" text-white">
+            <span className="countdown font-mono text-lg">
+              <span style={{ "--value": days }}></span>
+            </span>
+            days
+          </div>
+          <div className=" text-white">
+            <span className="countdown font-mono text-lg">
+              <span style={{ "--value": hours }}></span>
+            </span>
+            hours
+          </div>
+          <div className=" text-white">
+            <span className="countdown font-mono text-lg">
+              <span style={{ "--value": minutes }}></span>
+            </span>
+            min
+          </div>
+          <div className=" text-white">
+            <span className="countdown font-mono text-lg">
+              <span style={{ "--value": seconds }}></span>
+            </span>
+            sec
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- Frontend/dcc_contest_frontend/pages/contest/index.jsx
+++ Frontend/dcc_contest_frontend/pages/contest/index.jsx
@@ -10,209 +10,249 @@ import Countdown from "../../components/Countdown";
 import moment from "moment/moment";
 import ContestRegisterModal from "../../components/ContestRegisterModal";
 
-
 const contestPage = () => {
-    const router = useRouter();
-    const [open, setOpen] = useState(false);
-    const [name, setName] = useState("");
-    const [contestId, setContestId] = useState("");
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [contestId, setContestId] = useState("");
 
-    const ongoingContests = [
-        {
-            contestId: "A",
-            name: "Contest A",
-            startTime: "16/04/2023 00:00",
-            endTime: "20/04/2023 18:00",
-        },
-        {
-            contestId: "B",
-            name: "Contest B",
-            startTime: "15/04/2023 00:00",
-            endTime: "21/04/2023 09:00",
-        }
-    ];
+  const ongoingContests = [
+    {
+      contestId: "A",
+      name: "Contest A",
+      startTime: "16/04/2023 00:00",
+      endTime: "20/04/2023 18:00",
+    },
+    {
+      contestId: "B",
+      name: "Contest B",
+      startTime: "15/04/2023 00:00",
+      endTime: "21/04/2023 09:00",
+    },
+  ];
 
-    const upcomingContests = [
-        {
-            contestId: "C",
-            name: "Contest C",
-            startTime: "22/04/2023 00:00",
-            endTime: "25/04/2023 00:00",
-        },
-        {
-            contestId: "D",
-            name: "Contest D",
-            startTime: "25/04/2023 00:00",
-            endTime: "28/04/2023 00:00",
-        },
-    ];
+  const upcomingContests = [
+    {
+      contestId: "C",
+      name: "Contest C",
+      startTime: "22/04/2023 00:00",
+      endTime: "25/04/2023 00:00",
+    },
+    {
+      contestId: "D",
+      name: "Contest D",
+      startTime: "25/04/2023 00:00",
+      endTime: "28/04/2023 00:00",
+    },
+  ];
 
-    const pastContests = [
-        {
-            contestId: "E",
-            name: "Contest E",
-            startTime: "10/04/2023 00:00",
-            endTime: "15/04/2023 00:00",
-        },
-        {
-            contestId: "F",
-            name: "Contest F",
-            startTime: "10/04/2023 00:00",
-            endTime: "15/04/2023 00:00",
-        },
-        {
-            contestId: "G",
-            name: "Contest G",
-            startTime: "10/04/2023 00:00",
-            endTime: "15/04/2023 00:00",
-        },
-        {
-            contestId: "H",
-            name: "Contest H",
-            startTime: "10/04/2023 00:00",
-            endTime: "15/04/2023 00:00",
-        },
-    ];
+  const pastContests = [
+    {
+      contestId: "E",
+      name: "Contest E",
+      startTime: "10/04/2023 00:00",
+      endTime: "15/04/2023 00:00",
+    },
+    {
+      contestId: "F",
+      name: "Contest F",
+      startTime: "10/04/2023 00:00",
+      endTime: "15/04/2023 00:00",
+    },
+    {
+      contestId: "G",
+      name: "Contest G",
+      startTime: "10/04/2023 00:00",
+      endTime: "15/04/2023 00:00",
+    },
+    {
+      contestId: "H",
+      name: "Contest H",
+      startTime: "10/04/2023 00:00",
+      endTime: "15/04/2023 00:00",
+    },
+  ];
 
-    useEffect(() => {
+  useEffect(() => {
+    toggleLoaderBackdrop();
+
+    axios
+      .get(`${baseUrl}/contest`)
+      .then((res) => {
+        // divide as upcoming, ongoing and past contests and display on the screen
+        console.log(res);
+        console.log("Here at contest folder");
         toggleLoaderBackdrop();
+      })
+      .catch((err) => {
+        // do something based on error
+        console.log(err);
+        console.log(123);
+        toggleLoaderBackdrop();
+      });
+  }, []);
 
-        axios
-            .get(`${baseUrl}/contest`)
-            .then((res) => {
-                // divide as upcoming, ongoing and past contests and display on the screen
-                console.log(res);
-                console.log("Here at contest folder");
-                toggleLoaderBackdrop();
-            })
-            .catch((err) => {
-                // do something based on error
-                console.log(err);
-                console.log(123);
-                toggleLoaderBackdrop();
-            });
-    }, []);
+  function getDuration(startTime, endTime) {
+    const end = moment(endTime, "DD/MM/YYYY h:mm:s");
+    const start = moment(startTime, "DD/MM/YYYY h:mm:s");
 
+    const time = end.diff(start);
 
-    function getDuration(startTime, endTime) {
-        const end = moment(endTime, 'DD/MM/YYYY h:mm:s');
-        const start = moment(startTime, 'DD/MM/YYYY h:mm:s');
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((time / 1000 / 60) % 60);
 
+    return `${("0" + days).slice(-2)}:${("0" + hours).slice(-2)}:${(
+      "0" + minutes
+    ).slice(-2)}`;
+  }
+  return (
+    <>
+      <Head>
+        <title>Contests</title>
+      </Head>
 
-        const time = end.diff(start);
+      <Navbar />
 
-        const days = (Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = (Math.floor((time / (1000 * 60 * 60)) % 24));
-        const minutes = (Math.floor((time / 1000 / 60) % 60));
+      <div className="contest-page">
+        <ContestRegisterModal
+          open={open}
+          setOpen={setOpen}
+          contestId={contestId}
+          name={name}
+        />
+        <h1 id="contest-main-heading">CONTESTS</h1>
+        <div id="contest-section">
+          <h2 id="contest-heading">Ongoing Contests</h2>
+          <div className="overflow-x-auto">
+            <table className="table w-full custom-table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>S.N.</th>
+                  <th>Contest Name</th>
+                  <th>Start</th>
+                  <th>Length</th>
+                  <th>Contest Ends In</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {ongoingContests.map((contest, index) => (
+                  <tr key={contest.contestId} className="hover">
+                    <th>{index + 1}</th>
+                    <td>{contest.name}</td>
+                    <td>{contest.startTime}</td>
+                    <td>{getDuration(contest.startTime, contest.endTime)}</td>
+                    <td>
+                      <Countdown deadline={contest.endTime} />
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-outline btn-info min-h-8 h-8"
+                        onClick={() => {
+                          setOpen(true);
+                          setName(contest.name);
+                          setContestId(contest.contestId);
+                        }}
+                      >
+                         Enter 
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
 
-        return `${("0" + days).slice(-2)}:${("0" + hours).slice(-2)}:${("0" + minutes).slice(-2)}`;
+        <div id="contest-section">
+          <h2 id="contest-heading">Upcoming Contests</h2>
+          <div className="overflow-x-auto">
+            <table className="table w-full custom-table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>S.N.</th>
+                  <th>Contest Name</th>
+                  <th>Start</th>
+                  <th>Length</th>
+                  <th>Registration Closes in</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {upcomingContests.map((contest, index) => (
+                  <tr key={contest.contestId} className="hover">
+                    <th>{index + 1}</th>
+                    <td>{contest.name}</td>
+                    <td>{contest.startTime}</td>
+                    <td>{getDuration(contest.startTime, contest.endTime)}</td>
+                    <td>
+                      <Countdown deadline={contest.startTime} />
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-outline btn-info min-h-8 h-8"
+                        onClick={() => {
+                          setOpen(true);
+                          setName(contest.name);
+                          setContestId(contest.contestId);
+                        }}
+                      >
+                        Register
+                      </button>
+                    </td>{" "}
+                    {/*create a register modal here*/}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
 
-    }
-    return (
-        <>
-            <Head>
-                <title>Contests</title>
-            </Head>
-
-            <Navbar />
-            
-            <div className="contest-page">
-                <ContestRegisterModal open={open} setOpen={setOpen} contestId={contestId} name={name}/>
-                <h1 id="contest-main-heading">CONTESTS</h1>
-                <div id="contest-section">
-                    <h2 id="contest-heading">Ongoing Contests</h2>
-                    <div className="overflow-x-auto">
-                        <table className="table w-full custom-table">
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                    <th>S.N.</th>
-                                    <th>Contest Name</th>
-                                    <th>Start</th>
-                                    <th>Length</th>
-                                    <th>Time Left</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {ongoingContests.map((contest, index) => (
-                                    <tr key={contest.contestId} className="hover">
-                                        <th>{index + 1}</th>
-                                        <td>{contest.name}</td>
-                                        <td>{contest.startTime}</td>
-                                        <td>{getDuration(contest.startTime, contest.endTime)}</td>
-                                        <td><Countdown deadline={contest.endTime} /></td>
-                                    </tr>
-                                ))}
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-                <div id="contest-section">
-                    <h2 id="contest-heading">Upcoming Contests</h2>
-                    <div className="overflow-x-auto">
-                        <table className="table w-full custom-table">
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                    <th>S.N.</th>
-                                    <th>Contest Name</th>
-                                    <th>Start</th>
-                                    <th>Length</th>
-                                    <th>Registration Closes in</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {upcomingContests.map((contest, index) => (
-                                    <tr key={contest.contestId} className="hover">
-                                        <th>{index + 1}</th>
-                                        <td>{contest.name}</td>
-                                        <td>{contest.startTime}</td>
-                                        <td>{getDuration(contest.startTime, contest.endTime)}</td>
-                                        <td><Countdown deadline={contest.startTime} /></td>
-                                        <td><button className="btn btn-outline btn-info min-h-8 h-8"onClick={()=>{setOpen(true); setName(contest.name); setContestId(contest.contestId)}}>Register</button></td> { /*create a register modal here*/}
-                                    </tr>
-                                ))}
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-                <div id="contest-section">
-                    <h2 id="contest-heading">Past Contests</h2>
-                    <div className="overflow-x-auto">
-                        <table className="table w-full custom-table">
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                    <th>Sl. No.</th>
-                                    <th>Contest Name</th>
-                                    <th>Start</th>
-                                    <th>Length</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {pastContests.map((contest, index) => (
-                                    <tr key={contest.contestId} className="hover">
-                                        <th>{index + 1}</th>
-                                        <td>{contest.name}</td>
-                                        <td>{contest.startTime}</td>
-                                        <td>{getDuration(contest.startTime, contest.endTime)}</td>
-                                        <td><button className="btn btn-outline btn-success min-h-8 h-8" ><Link href={`/leaderboard/${contest.contestId}`} T>Leaderboard</Link></button> <button className="btn btn-outline btn-success min-h-8 h-8"><Link href={`/contest/${contest.contestId}`}>Problems</Link></button></td>
-                                    </tr>
-                                ))}
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+        <div id="contest-section">
+          <h2 id="contest-heading">Past Contests</h2>
+          <div className="overflow-x-auto">
+            <table className="table w-full custom-table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Sl. No.</th>
+                  <th>Contest Name</th>
+                  <th>Start</th>
+                  <th>Length</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {pastContests.map((contest, index) => (
+                  <tr key={contest.contestId} className="hover">
+                    <th>{index + 1}</th>
+                    <td>{contest.name}</td>
+                    <td>{contest.startTime}</td>
+                    <td>{getDuration(contest.startTime, contest.endTime)}</td>
+                    <td>
+                      <button className="btn btn-outline btn-success min-h-8 h-8">
+                        <Link href={`/leaderboard/${contest.contestId}`} T>
+                          Leaderboard
+                        </Link>
+                      </button>{" "}
+                      <button className="btn btn-outline btn-success min-h-8 h-8">
+                        <Link href={`/contest/${contest.contestId}`}>
+                          Problems
+                        </Link>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default contestPage;
