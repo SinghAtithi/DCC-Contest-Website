24f750847a59a81f3622122e85d9b1fa4cdc40ad
Ritik Kaushal <ritikkaushallvb@gmail.com>
2023-05-15 22:55:05 +0530

    Changed backend url

--- Frontend/dcc_contest_frontend/utils/constants.js
+++ Frontend/dcc_contest_frontend/utils/constants.js
@@ -38,7 +38,7 @@ export const END_USER = "end_user";
 
 export const ADMIN_DASHBOARD = "/admin/dashboard";
 
-export const BASE_URL = "https://6afe-4-240-84-221.ngrok-free.app";
+export const BASE_URL = "https://97ae-4-240-84-221.ngrok-free.app";
 // export const BASE_URL = "http://localhost:5000";
 export const LOGIN_ENDPOINT_BACKEND = "/auth/login";
 export const SIGNUP_ENDPOINT_BACKEND = "/auth/register";
