bb6aa562e868263a058b22815d63bada5d686f62
aryan20022003 <aryannita20022003@gmail.com>
2023-10-18 19:40:59 +0530

    reverting the changes the front end

--- Frontend/dcc_contest_frontend/components/ProblemStructure/HotTopics.jsx
+++ Frontend/dcc_contest_frontend/components/ProblemStructure/HotTopics.jsx
@@ -6,7 +6,7 @@ import { problemSet, problemDescription } from "../../utils/fakeData/data";
 function getDay() {
   const target = new Date(2023, 9, 17);
   const todayIstMil =
-    new Date().getTime() + 5 * 60 * 60 * 1000 + 30 * 60 * 1000;
+    new Date().getTime();
   const todayIst = new Date(todayIstMil);
   const diff = Math.floor((todayIst - target) / (1000 * 60 * 60 * 24));
   // console.log(diff);
--- Frontend/dcc_contest_frontend/components/ProblemStructure/ProblemView.jsx
+++ Frontend/dcc_contest_frontend/components/ProblemStructure/ProblemView.jsx
@@ -9,7 +9,7 @@ import { progressBar } from "../../utils/helper/apiIntegration";
 function getDay() {
   const target = new Date(2023, 9, 17);
   const todayIstMil =
-    new Date().getTime() + 5 * 60 * 60 * 1000 + 30 * 60 * 1000;
+    new Date().getTime() ;
   const todayIst = new Date(todayIstMil);
   const diff = Math.floor((todayIst - target) / (1000 * 60 * 60 * 24));
   // console.log(diff);
