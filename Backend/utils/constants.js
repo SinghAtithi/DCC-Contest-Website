3ddf16078b024512ff0a7138ea9ebe95b8bb84fa
aryan20022003 <aryannita20022003@gmail.com>
2023-10-23 19:50:04 +0530

    backend url updated in consts

--- Backend/utils/constants.js
+++ Backend/utils/constants.js
@@ -1,4 +1,5 @@
 const BASE_URL = "https://code-dcc.tech";
 const COMPILER_API="https://1260-20-197-41-122.ngrok-free.app";
-const BACKEND_URL="https://upset-yak-earmuffs.cyclic.app";
+// const BACKEND_URL="https://upset-yak-earmuffs.cyclic.app";
+const BACKEND_URL="https://wide-eyed-crab-gabardine.cyclic.app/"
 module.exports = { BASE_URL , BACKEND_URL,COMPILER_API};
