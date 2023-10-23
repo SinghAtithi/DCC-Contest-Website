d81608b1201e6bfbf27e5c07c44b822a3545d788
aryan <114981725+Aryan20022003@users.noreply.github.com>
2023-10-23 18:04:58 +0530

    update: Update backEnd url  (#120)
    * backend url updated

--- Backend/utils/constants.js
+++ Backend/utils/constants.js
@@ -1,3 +1,3 @@
 const BASE_URL = "https://code-dcc.tech";
-const BACKEND_URL="https://ill-pear-dalmatian-slip.cyclic.app"
+const BACKEND_URL="https://upset-yak-earmuffs.cyclic.app";
 module.exports = { BASE_URL , BACKEND_URL};
