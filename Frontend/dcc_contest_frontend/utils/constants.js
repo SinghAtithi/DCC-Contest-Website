f4d07519a636c80afc884937a14000ba091b428a
Ritik-Kaushal <ritikkaushallvb@gmail.com>
2023-10-23 17:17:27 +0530

    Cyclic new url

--- Frontend/dcc_contest_frontend/utils/constants.js
+++ Frontend/dcc_contest_frontend/utils/constants.js
@@ -34,7 +34,7 @@ export const END_USER = "end_user";
 
 export const ADMIN_DASHBOARD = "/admin/dashboard";
 
-export const BASE_URL = "https://ill-pear-dalmatian-slip.cyclic.app";
+export const BASE_URL = "https://upset-yak-earmuffs.cyclic.app";
 //export const BASE_URL = "http://localhost:5000";
 
 export const LOGIN_ENDPOINT_BACKEND = "/auth/login";
