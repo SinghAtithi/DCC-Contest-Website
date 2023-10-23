a68963af2106a7fc53ae3d6094d429f5272024f1
aryan20022003 <aryannita20022003@gmail.com>
2023-10-23 19:20:10 +0530

    compiler api added

--- Backend/utils/constants.js
+++ Backend/utils/constants.js
@@ -1,3 +1,4 @@
 const BASE_URL = "https://code-dcc.tech";
+const COMPILER_API="https://1260-20-197-41-122.ngrok-free.app";
 const BACKEND_URL="https://upset-yak-earmuffs.cyclic.app";
-module.exports = { BASE_URL , BACKEND_URL};
+module.exports = { BASE_URL , BACKEND_URL,COMPILER_API};
