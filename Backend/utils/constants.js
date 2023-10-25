b6bdb4008f643f0e8a8db3bf54edb0c1e141d81e
aryan <114981725+Aryan20022003@users.noreply.github.com>
2023-10-25 12:09:52 +0530

    Aryan (#125)
    * url updated
    
    * backend-url updated
    
    * base url updated
    
    * typo fixed

--- Backend/utils/constants.js
+++ Backend/utils/constants.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://code-dcc.tech";
 const COMPILER_API="https://1260-20-197-41-122.ngrok-free.app";
-// const BACKEND_URL="https://upset-yak-earmuffs.cyclic.app";
-const BACKEND_URL="https://expensive-puce-handbag.cyclic.app";
+// const BACKEND_URL="https://dcc-code-backend-production.up.railway.app";
+const BACKEND_URL="https://b084-185-207-250-57.ngrok-free.app";
 module.exports = { BASE_URL , BACKEND_URL,COMPILER_API};
